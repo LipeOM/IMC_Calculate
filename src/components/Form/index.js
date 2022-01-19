751435560db32636c497495815e348cfff3ea2f2
Luiz Felipe O. Martins <38307413+LipeOM@users.noreply.github.com>
2022-01-19 15:45:53 -0400

    Add files via upload

---
+++ src/components/Form/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { View, TextInput, Text, TouchableOpacity, Vibration, Pressable, Keyboard } from "react-native";
+import ResultImc from "./ResultImc/";
+import styles from "./style";
+
+export default function Form(){
+
+const [height, setHeight] = useState(null)
+const [weight, setWeight] = useState(null)
+const [messageImc, setMessageImc] = useState("preencha o peso e altura")  
+const [imc, setImc] = useState(null)  
+const [textButton, setTextButton] = useState("calcular")   
+const [errorMessage, setErrorMessage] = useState(null) 
+
+function imcCalculador(){
+    return setImc((weight/(height*height)).toFixed(2))
+}
+
+function verificationImc(){
+    if(imc == null){
+        Vibration.vibrate();
+        setErrorMessage("campo obrigatório*")
+    }
+}
+
+function validationImc(){
+    if(weight != null && height != null){
+        imcCalculador();
+        setHeight(null)
+        setWeight(null)
+        setMessageImc("Seu imc é igual: ")
+        setTextButton("Calcular Novamente")
+        setErrorMessage(null)
+    } else {
+        verificationImc()
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("preencha o peso e altura")
+    }
+    
+}
+
+    return (
+        <Pressable onPress={Keyboard.dismiss} style ={styles.formContext}>
+            <View style = {styles.form}>
+                <Text style = {styles.formLabel} >Altura</Text>
+                <Text style = {styles.errorMessage} >{errorMessage}</Text>
+                <TextInput style = {styles.input} onChangeText={setHeight} value={height} placeholder = "Ex. 1.75" keyboardType="numeric" />
+
+                <Text style = {styles.formLabel} >Peso</Text>
+                <Text style = {styles.errorMessage} >{errorMessage}</Text>
+                <TextInput style = {styles.input} onChangeText={setWeight} value={weight} placeholder = "Ex. 75.7" keyboardType="numeric" />
+
+                <TouchableOpacity style={styles.buttonCalculator} onPress={() => validationImc()}> 
+                    <Text style={styles.textButtonCalculator} >{textButton}</Text>
+                </TouchableOpacity>
+            </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc} />
+        </Pressable>
+    );
+}
